c5b56e39c01fd87184cc375b98d4edaf95f8b602
Ionut Mocanu <ionutmocanu@Ionuts-MacBook-Pro.local>
2017-02-24 13:42:53 +0200

    Remove duplicates

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ import minify from 'node-json-minify'
 
 (async() => {
     const allQuotes = await getQuoteCategories()
-    fs.writeFileSync(`./data/Quotes.json`, minify(JSON.stringify(allQuotes, null, 2)), 'utf8')
+
+	var tempObj = [];
+	var quotesJSON = {};
+	var seenNames = {};
+	
+	tempObj = Object.keys(allQuotes).filter(function(currentObject) {
+	    if (allQuotes[currentObject].quote in seenNames) {
+	        return false;
+	    } else {
+	        seenNames[allQuotes[currentObject].quote] = true;
+	        quotesJSON[currentObject] = allQuotes[currentObject];
+	        return true;
+	    }
+	});
+
+    fs.writeFileSync(`./data/Quotes.json`, minify(JSON.stringify(quotesJSON, null, 2)), 'utf8')
     console.log('\n>> Quotes Categories Retrieved!\n');
 })()
